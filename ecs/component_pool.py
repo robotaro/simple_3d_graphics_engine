e5728e2804535deb1b3109078c50fe54a5b0a5a9
Alexandre Vicente <alexandrepv@gmail.com>
2023-09-24 16:19:06 +0100

    Moved scene loading function to component pool as it is more semantically coherent

--- ecs/component_pool.py
+++ ecs/component_pool.py
@@ -1,4 +1,5 @@
-from typing import Union
+import os
+from bs4 import BeautifulSoup
 
 from ecs import constants
 from ecs.components.component import Component
@@ -11,6 +12,8 @@ from ecs.components.input_control import InputControl
 from ecs.components.text_2d import Text2D
 from ecs.components.point_light import PointLight
 from ecs.components.directional_light import DirectionalLight
+
+from ecs.utilities import utils_string
 
 
 class Entity:
@@ -70,6 +73,8 @@ class ComponentPool:
             constants.COMPONENT_TYPE_POINT_LIGHT: self.point_light_components,
         }
 
+
+
     def create_entity(self, name="") -> int:
         uid = self.entity_uid_counter
         self.entities[uid] = Entity(name=name)
@@ -115,5 +120,208 @@ class ComponentPool:
 
     def get_entities_using_component(self, component_type: int) -> list:
         return list(self.component_storage_map[component_type].keys())
+
+    # ================================================================================
+    #                           Scene Loading Functions
+    # ================================================================================
+
+    def load_scene(self, scene_xml_fpath: str):
+
+        # Check if path is absolute
+        fpath = None
+        if os.path.isfile(scene_xml_fpath):
+            fpath = scene_xml_fpath
+
+        if fpath is None:
+            # Assume it is a relative path from the working directory/root directory
+            clean_scene_xml_fpath = scene_xml_fpath.replace("\\", os.sep).replace("/", os.sep)
+            fpath = os.path.join(constants.ROOT_DIR, clean_scene_xml_fpath)
+
+        # Load UI window blueprint
+        with open(fpath) as file:
+            root_soup = BeautifulSoup(file.read(), features="lxml")
+
+            # Find window root node - there should be only one
+            ui_soup = root_soup.find("scene")
+            if ui_soup is None:
+                raise ValueError(f"[ERROR] Could not find root 'scene' element")
+
+            for entity_soup in root_soup.find_all("entity"):
+
+                entity_name = entity_soup.attrs.get("name", "unamed_entity")
+                entity_uid = self.create_entity(name=entity_name)
+
+                self._add_entity_components(entity_uid=entity_uid, entity_soup=entity_soup)
+
+    def _add_entity_components(self, entity_uid: int, entity_soup: BeautifulSoup) -> None:
+        """
+        This function uses the Beautifulsoup element provided to add all the components
+        assigned to the entity UID.
+        :param entity_uid: int,
+        :param entity_soup:
+        :return: None
+        """
+
+        for component_soup in entity_soup.find_all():
+
+            # Transform 3D
+            if component_soup.name == constants.COMPONENT_NAME_TRANSFORM_3D:
+                position_str = component_soup.attrs.get("position", "0 0 0")
+                position = utils_string.string2float_list(position_str)
+
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_TRANSFORM_3D,
+                    position=position)
+                continue
+
+            # Transform 2D
+            if component_soup.name == constants.COMPONENT_NAME_TRANSFORM_2D:
+                continue
+
+            # Mesh
+            if component_soup.name == constants.COMPONENT_NAME_MESH:
+                shape = component_soup.attrs.get("shape", None)
+                if shape is None:
+                    raise Exception("You need to specify the shape of the mesh you want to create.")
+
+                # Shape: OBJ
+                if shape == constants.MESH_SHAPE_FROM_OBJ:
+                    fpath = component_soup.attrs.get("fpath", None)
+                    if shape is None:
+                        raise Exception("You need to specify the location of the .obj file")
+
+                    self.add_component(
+                        entity_uid=entity_uid,
+                        component_type=constants.COMPONENT_TYPE_MESH,
+                        shape=shape,
+                        fpath=fpath)
+
+                # Shape: BOX
+                if shape == constants.MESH_SHAPE_BOX:
+                    self.add_component(
+                        entity_uid=entity_uid,
+                        component_type=constants.COMPONENT_TYPE_MESH,
+                        shape=shape,
+                        width=float(component_soup.attrs.get("width", "1.0")),
+                        height=float(component_soup.attrs.get("height", "1.0")),
+                        depth=float(component_soup.attrs.get("depth", "1.0")))
+
+                # Shape: ICOSPHERE
+                if shape == constants.MESH_SHAPE_ICOSPHERE:
+                    self.add_component(
+                        entity_uid=entity_uid,
+                        component_type=constants.COMPONENT_TYPE_MESH,
+                        shape=shape,
+                        radius=float(component_soup.attrs.get("radius", "0.2")),
+                        subdivisions=int(component_soup.attrs.get("subdivisions", "3")))
+                continue
+
+            # Camera
+            if component_soup.name == constants.COMPONENT_NAME_CAMERA:
+                viewport_norm_str = component_soup.attrs.get("viewport_ratio", "0.0 0.0 1.0 1.0")
+                perspective_str = component_soup.attrs.get("perspective", "true")
+
+                viewport_norm = tuple(utils_string.string2int_list(viewport_norm_str))
+                perspective = utils_string.str2bool(perspective_str)
+
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_CAMERA,
+                    viewport_norm=viewport_norm,
+                    perspective=perspective)
+                continue
+
+            # Renderable
+            if component_soup.name == constants.COMPONENT_NAME_RENDERABLE:
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_RENDERABLE)
+                continue
+
+            # Material
+            if component_soup.name == constants.COMPONENT_NAME_MATERIAL:
+
+                diffuse_str = component_soup.attrs.get("diffuse", ".75 .75 .75")
+                specular_str = component_soup.attrs.get("ambient", "1.0 1.0 1.0")
+
+                diffuse = tuple(utils_string.string2float_list(diffuse_str))
+                specular = tuple(utils_string.string2float_list(specular_str))
+
+                shininess_factor = float(component_soup.attrs.get("shininess_factor", "32.0"))
+                metallic_factor = float(component_soup.attrs.get("metallic_factor", "1.0"))
+                roughness_factor = float(component_soup.attrs.get("roughness_factor", "0.0"))
+
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_MATERIAL,
+                    diffuse=diffuse,
+                    specular=specular,
+                    shininess_factor=shininess_factor,
+                    metallic_factor=metallic_factor,
+                    roughness_factor=roughness_factor)
+
+                continue
+
+            # Input Control
+            if component_soup.name == constants.COMPONENT_NAME_INPUT_CONTROL:
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_INPUT_CONTROL)
+                continue
+
+            # Text 2D
+            if component_soup.name == constants.COMPONENT_NAME_TEXT_2D:
+                font_name = component_soup.attrs.get("font_name", None)
+                if font_name is None:
+                    raise Exception("You need to specify the font")
+
+                text_component = self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_TEXT_2D,
+                    font_name=font_name)
+
+                # Add text, if present
+                text = entity_soup.text.strip()
+                if len(text) > 0:
+                    text_component.set_text(text=text)
+                continue
+
+            # Directional Light
+            if component_soup.name == constants.COMPONENT_NAME_DIRECTIONAL_LIGHT:
+                diffuse_str = component_soup.attrs.get("diffuse", "1.0 1.0 1.0")
+                specular_str = component_soup.attrs.get("ambient", "1.0 1.0 1.0")
+
+                diffuse = tuple(utils_string.string2float_list(diffuse_str))
+                specular = tuple(utils_string.string2float_list(specular_str))
+
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_DIRECTIONAL_LIGHT,
+                    diffuse=diffuse,
+                    specular=specular)
+                continue
+
+            # Point Light
+            if component_soup.name == constants.COMPONENT_NAME_POINT_LIGHT:
+                position_str = component_soup.attrs.get("position", "22.0 16.0 50.0")
+                color_str = component_soup.attrs.get("color", "1.0 1.0 1.0")
+
+                position = utils_string.string2float_list(position_str)
+                color = utils_string.string2float_list(color_str)
+
+                self.add_component(
+                    entity_uid=entity_uid,
+                    component_type=constants.COMPONENT_TYPE_POINT_LIGHT,
+                    position=position,
+                    color=color)
+                continue
+
+            # If you got here, it means the component you selected is not supported :(
+            entity_name = entity_soup.attrs.get("name", "")
+            if len(entity_name) > 0:
+                entity_name = f" ({entity_name})"
+                self.logger.error(f"Component {component_soup.name}, declared in entity uid "
+                                  f"{entity_uid}{entity_name}, is not supported.")
 
 
