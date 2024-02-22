c62675569bb905bc677cd37a9528578095676a5b
Alexandre Vicente <alexandrepv@gmail.com>
2024-02-22 23:01:20 +0000

    Updating meshes and mesh factory to rely on data_groups and data_blocks

--- src2/components/mesh.py
+++ src2/components/mesh.py
@@ -8,16 +8,18 @@ from src2.components.component import Component
 class Mesh(Component):
 
     __slots__ = [
+        "data_blocks",
         "vaos",
         "vbos",
         "ibo_indices",
         "render_mode",
         "resource_id",
+        "shape",
         "layer",
         "visible"]
 
     VBO_DECLARATION_MAP = {
-        #           (buffer_data_type, buffer_data_size, shader_variable)
+        # key       (buffer_data_type, buffer_data_size, shader_variable)
         "vertices": ("f4", "3f", constants.SHADER_INPUT_VERTEX),
         "normals": ("f4", "3f", constants.SHADER_INPUT_NORMAL),
         "colors": ("f4", "3f", constants.SHADER_INPUT_COLOR),
@@ -31,6 +33,7 @@ class Mesh(Component):
         super().__init__(**kwargs)
 
         # GPU (VRAM) Vertex Data
+        self.data_blocks = {}
         self.vaos = {}
         self.vbos = {}
         self.ibo_indices = None
@@ -38,8 +41,9 @@ class Mesh(Component):
         # Get parameters
         self.resource_id = self.params.get("resource_id", None)
         self.render_mode = self.params.get("render_mode", constants.MESH_RENDER_MODE_TRIANGLES)
+        self.shape = self.params.get(constants.COMPONENT_ARG_MESH_SHAPE, None)
 
-        #self.create_mesh()
+        self.create_mesh()
 
 
         data_group = self.data_manager.data_groups.get(self.resource_id, None)
@@ -88,9 +92,7 @@ class Mesh(Component):
     def upload_buffer_data(self, buffer_name: str, data: np.ndarray):
         self.vbos[buffer_name].write(data.tobytes())
 
-    def create_mesh(self) -> None:
-
-        shape = self.params.get(constants.COMPONENT_ARG_MESH_SHAPE, None)
+    def create_mesh(self, shape: str) -> None:
 
         if shape is None:
             raise Exception("[ERROR] Both resource_id and shape specification are None. Please provide one.")
@@ -101,42 +103,29 @@ class Mesh(Component):
             width = self.params.get("width", 1.0)
             height = self.params.get("height", 1.0)
             depth = self.params.get("depth", 1.0)
-
             mesh = mesh_factory.create_box(width=width, height=height, depth=depth)
-            self.vertices = mesh[0]
-            self.normals = mesh[1]
-            self.indices = mesh[3]
-            return
 
-        if shape == constants.MESH_SHAPE_ICOSPHERE:
+        elif shape == constants.MESH_SHAPE_ICOSPHERE:
             radius = self.params.get("radius", 0.5)
             subdivisions = self.params.get("subdivisions", 3)
-
             mesh = mesh_factory.create_icosphere(radius=radius, subdivisions=subdivisions)
-            self.vertices = mesh[0]
-            self.normals = mesh[1]
-            self.indices = mesh[3]
-            return
 
-        if shape == constants.MESH_SHAPE_CAPSULE:
+        elif shape == constants.MESH_SHAPE_CAPSULE:
             height = self.params.get("height", 1.0)
             radius = self.params.get("radius", 0.25)
             count = self.params.get("count", (16, 16))
-
             mesh = mesh_factory.create_capsule(height=height, radius=radius, count=count)
-            self.vertices = mesh[0]
-            self.normals = mesh[1]
-            self.indices = mesh[3]
-            return
 
-        if shape == constants.MESH_SHAPE_CYLINDER:
+        elif shape == constants.MESH_SHAPE_CYLINDER:
             point_a = self.params.get("point_a", (0.0, 0.0, 0.0))
             point_b = self.params.get("point_b", (0.0, 1.0, 0.0))
             radius = self.params.get("radius", 0.5)
             sections = self.params.get("sections", 32)
-
             mesh = mesh_factory.create_cylinder(point_a=point_a, point_b=point_b, sections=sections, radius=radius)
-            self.vertices = mesh[0]
-            self.normals = mesh[1]
-            self.indices = mesh[3]
-            return
+
+        else:
+            raise Exception(f"[ERROR] Shape '{shape}' is not supported")
+
+        self.vbos["vertices"] = mesh[0]
+        self.vbos["normals"] = mesh[1]
+        self.vbos["indices"] = mesh[3]
